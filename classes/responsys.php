381cfa66e7887d81dc008ab124acf3f262ad1878
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2015-06-22 16:44:52 +0300

    fixes for PHP 5.3

--- classes/responsys.php
+++ classes/responsys.php
@@ -238,7 +238,7 @@ class Responsys
         );
         $headers       = array_merge($defaultHeders, $headers);
 
-        $dataJSON = json_encode($data, JSON_PRETTY_PRINT);
+        $dataJSON = json_encode($data);
         // Fix double slashes for unicode sequences
         $dataJSON = preg_replace('/\\\\\\\\u([0-9a-fA-F]{4})/u', '\u$1', $dataJSON);
         switch ($method) {
@@ -267,7 +267,7 @@ class Responsys
 
         $log = new ResponsysLog();
         $log->setAttribute('request_uri', $uri);
-        $log->setAttribute('request', $dataJSON);
+        $log->setAttribute('request', self::json_pretty($dataJSON));
         $log->setAttribute('request_headers', implode("\n", $headers));
         $log->setAttribute('response_status', $info['http_code']);
         $log->setAttribute('response_headers', $header);
@@ -305,6 +305,82 @@ class Responsys
             throw new Exception($error);
         }
 
+        return $result;
+    }
+
+    /**
+     * Pretty-print JSON string
+     *
+     * Use 'format' option to select output format - currently html and txt supported, txt is default
+     * Use 'indent' option to override the indentation string set in the format - by default for the 'txt' format it's a tab
+     *
+     * @param string $json Original JSON string
+     * @param array $options Encoding options
+     * @return string
+     */
+    protected static function json_pretty($json, $options = array())
+    {
+        $tokens = preg_split('|([\{\}\]\[,])|', $json, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $result = '';
+        $indent = 0;
+
+        $format = 'txt';
+
+        //$ind = "\t";
+        $ind = "    ";
+
+        if (isset($options['format'])) {
+            $format = $options['format'];
+        }
+
+        switch ($format) {
+            case 'html':
+                $lineBreak = '<br />';
+                $ind       = '&nbsp;&nbsp;&nbsp;&nbsp;';
+                break;
+            default:
+            case 'txt':
+                $lineBreak = "\n";
+                //$ind = "\t";
+                $ind       = "    ";
+                break;
+        }
+
+        // override the defined indent setting with the supplied option
+        if (isset($options['indent'])) {
+            $ind = $options['indent'];
+        }
+
+        $inLiteral = false;
+        foreach ($tokens as $token) {
+            if ($token == '') {
+                continue;
+            }
+
+            $prefix = str_repeat($ind, $indent);
+            if (!$inLiteral && ($token == '{' || $token == '[')) {
+                $indent++;
+                if (($result != '') && ($result[(strlen($result) - 1)] == $lineBreak)) {
+                    $result .= $prefix;
+                }
+                $result .= $token . $lineBreak;
+            } elseif (!$inLiteral && ($token == '}' || $token == ']')) {
+                $indent--;
+                $prefix = str_repeat($ind, $indent);
+                $result .= $lineBreak . $prefix . $token;
+            } elseif (!$inLiteral && $token == ',') {
+                $result .= $token . $lineBreak;
+            } else {
+                $result .= ( $inLiteral ? '' : $prefix ) . $token;
+
+                // Count # of unescaped double-quotes in token, subtract # of
+                // escaped double-quotes and if the result is odd then we are
+                // inside a string literal
+                if ((substr_count($token, "\"") - substr_count($token, "\\\"")) % 2 != 0) {
+                    $inLiteral = !$inLiteral;
+                }
+            }
+        }
         return $result;
     }
 }
